16a7f486adcfa9fac6565a072af7978cd4163ece
ibrahim cicek <ibrahimcicek.6405@gmail.com>
2026-02-03 17:14:33 +0300

    Cors Ayari duzeltildi

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json()); // JSON verilerini okuyabilmek için
 app.use("/api/media", mediaRoutes);
 // MongoDB Bağlantısı
